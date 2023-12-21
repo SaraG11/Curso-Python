0972c1a09216bc4590a73876964d1190e68ecdef
SG-WORK <gpsara1114@gmail.com>
2023-12-21 11:59:12 -0600

    Hello Python

---
+++ hello.py
@@ -0,0 +1,23 @@
+#Hola mundo
+print("Hola Python")
+#Cadenas de texto con comilla doble o simple
+print('Hola Python')
+
+"""
+Este es un comentario
+en varias lineas
+
+"""
+
+'''
+Este tambien es un comentario
+en varias lineas
+
+'''
+#Consultar el tip de dato
+print(type('Soy un dato str')) #tipo 'str'
+print(type(5)) #tipo 'int'
+print(type(1.6)) #tipo 'float'
+print(type(1 + 2j)) #tipo 'complex'
+print(type(True)) #tipo 'bool'
+
