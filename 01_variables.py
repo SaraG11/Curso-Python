eaa1cc02b6870a137fb74ffd0de103e2bfd96444
SG-WORK <gpsara1114@gmail.com>
2023-12-22 09:47:47 -0600

    Variables Jueves

---
+++ 01_variables.py
@@ -0,0 +1,46 @@
+# Variables
+
+my_variable = 'My String variable'
+#print(my_variable)
+
+my_int_variable= 5
+#print(my_int_variable)
+
+my_bool_variable = True
+#print(my_bool_variable)
+
+#concatenacion de variables
+#print(my_variable, my_int_variable, my_bool_variable)
+
+#tipo de variable
+my_int_to_str_var = str(my_int_variable)
+#print(my_int_to_str_var)
+#print(type(my_int_to_str_var))
+
+#Funciones del sistema
+#print(len(my_variable))
+
+#print('Este es el valor de:', my_bool_variable)
+
+#variables en una sola linea
+name, subname, alias, edad = 'Sara', 'Gonzalez', 'SG', 25
+#print("Me llamo:", name, subname, 'Mi edad es:', edad, 'Mi alias es:', alias )
+
+#Input
+##first_name = input('¿Cual es tu nombre?: ')
+##age = input('¿Cuantos años tienes? ')
+'''
+print(name)
+print(edad)
+
+#cabiamos su tipo
+name= 35
+age= 'Nayeli'
+print(name)
+print(edad)
+'''
+
+#Forzamos el tipo
+adress: str = 'Mi direccion'
+adress = 32 
+print(type(adress))
