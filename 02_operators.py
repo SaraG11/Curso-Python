8c5335c686f3780f3a89fc1514acc8644ce4c1cd
SG-WORK <gpsara1114@gmail.com>
2023-12-22 11:32:43 -0600

    Operadores Viernes

---
+++ 02_operators.py
@@ -0,0 +1,38 @@
+# OPERADORES
+'''
+print(3 + 4)
+print(3 - 4)
+print(3 / 4)
+print(3 * 4)
+
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3)
+
+#CNCATENACION
+print('Hola ' + 'Python')
+
+#NO MEZCLA
+#print('Hola ' - 'Python')
+#print('Hola ' + 5)
+print('Hola ' + str(5))
+print('Hola ' * 5)
+
+my_float = 2.5 * 2
+print('Hola ' * int(my_float))
+
+
+print(3 > 4)    
+print(3 >= 4)   
+print(3 < 4)     
+print(2 < 4)    
+print(2 <= 4)   
+print(3 == 4)     
+print(3 != 4)
+'''
+##OPERADORES LÓGICOS ##
+print(3 > 4 and 'Hola ' > 'Python')
+print(3 > 4 or 'Hola ' > 'Python')
+print(3 < 4 and 'Hola ' < 'Python')
+print(3 < 4 or 'Hola ' > 'Python')
+print(not(3 > 4))  
