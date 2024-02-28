28c63aea29e861ed76901c2691a3b71accb01abc
SG-WORK <gpsara1114@gmail.com>
2024-02-27 18:56:54 -0600

    04_listas pt2

--- 04_lists.py
+++ 04_lists.py
@@ -25,6 +25,7 @@ print(my_other_list.count('Gonzalez'))
 print(my_list.count(30))  #retorna el numero de valor, cuantas veces se repite
 
 #desempaquetar
+"""
 #age, height, name = my_other_list ---- ERROR NECESITA TODOS LOS ELEMENTOS DE LA LISTA
 age, height, name, surname = my_other_list
 print(name)
@@ -34,11 +35,52 @@ print(name)
 
 print(my_list + my_other_list)
 
+"""
+
+#elementos de la lista 
+"""
+my_other_list.append("SG") #un append lo va insertar la final de una lista
+print(my_other_list)
+
+my_other_list.insert(1, "NARANJA") #en que posicion quiere que insertemos el valor
+print(my_other_list)
+
+my_other_list.remove("NARANJA") #elimina un valor que se desea
+print(my_other_list)
+
+my_list.remove(30) #elimina el primer valor aunque haya dos 
+print(my_list)
+"""
+
+#estructuras colas
+my_list.pop() #elimina el ultimo registro o desapila la lista 
+print(my_list.pop())
+
+
+my_pop_element = my_list.pop(2)
+print(my_pop_element)
+# print(my_list.pop(2)) el numero del elemento que se quiere desapilar recordando que la lista inicia con 0
+
+#eliminacion de un valor de la lista sin guardarlo por indice 
+del my_list[2]
+print(my_list)
+
+my_list.clear() #no habrá ningun elemneto de la lista
+print(my_list)
+
+my_other_list.insert(1, "NARANJA") #en que posicion quiere que insertemos el valor
+print(my_other_list)
+
+my_other_list[1] = 'Rojo'
+print(my_other_list)
+"""
 #tipos dinamicos
 my_list = 'Hola Py'
 print(my_list)
 
-my_list = ['Hola Py']
+my_list = ['Hola Py'] #esto ya es una lista con poner los corchetes
 print(my_list)
 print(type(my_list))
+"""
+
 
