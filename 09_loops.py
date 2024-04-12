5801bff2e1e5c184cfd8883af50ae25d09d20c28
SG-WORK <gpsara1114@gmail.com>
2024-04-11 18:56:11 -0600

    Part1.Loops

---
+++ 09_loops.py
@@ -0,0 +1,32 @@
+## Loops (Bucles, ciclos) ##
+
+#While (mientras sea verdadero haz algo)
+"""
+my_condition = 0
+
+while my_condition < 10:
+    print(my_condition) 
+    my_condition += 2 #incremento
+else: #es opcional
+    print("Mi condicion es mayor o igual a 10")
+
+print("La ejecución continúa")
+
+while my_condition < 20:
+    my_condition += 1 
+    if my_condition == 15:
+        print("Se detiene la ejecución")
+        break
+
+    print(my_condition)
+
+print("La ejecucion continúa")
+"""
+
+# For 
+
+my_list = [25, 24, 62, 52, 30, 30, 17]
+
+for element in my_list:
+    print(element)
+
