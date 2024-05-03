49a44112f82635918f4b78b850c788fe96d002d8
SG-WORK <gpsara1114@gmail.com>
2024-05-03 12:38:57 -0600

    Dates 02

--- Intermediate/00_dates.py
+++ Intermediate/00_dates.py
@@ -21,6 +21,7 @@ print(timestamp)
 #trabajar con una fecha en específico 
 year_2025 = datetime(2025, 1, 1)
 
+'''
 def print_date(date):
     print(date.year)
     print(date.month)
@@ -28,3 +29,38 @@ def print_date(date):
     print(date.hour)
     print(date.minute)
     print(date.second)
+'''
+
+from datetime import time
+
+current_time = time(21, 6, 0)
+
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
+
+from datetime import date
+
+current_date = date.today() #se puede tomar el día de ahora en curso
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+current_date = date(2022, 10, 6) #pero al igual se pueden pasar los parámetros de la fecha 
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+current_date = date(current_date.year, current_date.month + 1, current_date.day)
+
+print(current_date.month)
+
+from datetime import timedelta
+
+start_timedelta = timedelta(200, 100, 100, weeks= 10)
+
+end_timedelta = timedelta(300, 100, 100, weeks= 13)
+
+print(end_timedelta - start_timedelta)
+print(end_timedelta + start_timedelta)
