69afb6d949ade8f4f5eb12ba07ca7c9c4fba5eb2
SG-WORK <gpsara1114@gmail.com>
2024-05-13 18:40:47 -0600

    Ejercicio 2

--- Intermediate/02_challenges.py
+++ Intermediate/02_challenges.py
@@ -9,7 +9,7 @@ cada impresión), sustituyendo los siguientes:
 - Múltiplos de 3 por la palabra "fizz".
 - Múltiplos de 5 por la palabra "buzz".
 - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
-'''
+
 #creacion de funcion
 def fizzbuzz():
     for index in range(1, 101):
@@ -23,3 +23,21 @@ def fizzbuzz():
             print(index)
 
 fizzbuzz()
+'''
+
+'''
+ Escribe una función que reciba dos palabras (String) y retorne
+ verdadero o falso (Bool) según sean o no anagramas.
+    - Un Anagrama consiste en formar una palabra reordenando TODAS
+      las letras de otra palabra inicial.
+    - NO hace falta comprobar que ambas palabras existan.
+    - Dos palabras exactamente iguales no son anagrama.
+'''
+
+def is_anagram(word_one, word_two):
+    if word_one.lower() == word_two.lower():
+        return False
+    return word_one == word_two
+
+
+print(is_anagram('Amor', 'Roma'))
